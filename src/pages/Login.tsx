c9f6178095bd35d6ec3165de932dbcfea4e1c8ff
JefersonLC <c.jeferson.lc@gmail.com>
2023-05-22 23:06:38 -0500

    updated Login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,61 @@
+import GitHubIcon from '../components/icons/GitHubIcon';
+import GoogleIcon from '../components/icons/GoogleIcon';
+import AuthButton from '../components/login/AuthButton';
+import FooterWave from '../components/svg/FooterWave';
+import HeaderWave from '../components/svg/HeaderWave';
 import { useUser } from '../hooks/useUser';
 
 export default function Login() {
-  const { sigInWithGitHub } = useUser();
+  const { signInWithGitHub, signInWithGoogle } = useUser();
+
+  const authButtons = [
+    {
+      provider: 'github',
+      icon: <GitHubIcon />,
+      children: 'Iniciar Sesión con GitHub',
+      action: signInWithGitHub,
+    },
+    {
+      provider: 'google',
+      icon: <GoogleIcon />,
+      children: 'Iniciar Sesión con Google',
+      action: signInWithGoogle,
+    },
+  ];
 
   return (
-    <div>
-      <button onClick={sigInWithGitHub}>Login con GitHub</button>
+    <div className='min-h-screen relative flex items-center justify-center bg-russianViolet'>
+      <div className='absolute top-0 w-full'>
+        <HeaderWave />
+      </div>
+      <section className='mx-6 py-8 gap-24 max-w-[342px] bg-honeydew md:max-w-min z-10'>
+        <div className='px-6 md:px-8'>
+          <h2 className='font-[chocopy] text-4xl text-center md:text-5xl'>
+            ToDoApp
+          </h2>
+          <p className='mt-4 text-justify md:mt-6 md:text-lg'>
+            Bienvenido a nuestra aplicación, donde tu productividad es nuestra
+            prioridad. Convierte el caos en orden y deja que nuestro asistente
+            te guíe en la administración de tus tareas. Mejora tu enfoque y
+            logra más en menos tiempo.
+          </p>
+        </div>
+        <div className='mt-8 px-6 flex flex-col gap-4 items-center justify-center md:mt-10 md:px-8'>
+          {authButtons.map((button) => (
+            <AuthButton
+              key={button.provider}
+              provider={button.provider}
+              icon={button.icon}
+              action={button.action}
+            >
+              {button.children}
+            </AuthButton>
+          ))}
+        </div>
+      </section>
+      <div className='absolute bottom-0 w-full'>
+        <FooterWave />
+      </div>
     </div>
   );
 }
