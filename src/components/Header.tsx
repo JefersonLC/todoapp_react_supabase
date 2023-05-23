40cf1b5763919b58ab3c02971a37a04d6bc5ea73
JefersonLC <c.jeferson.lc@gmail.com>
2023-05-22 23:03:01 -0500

    added Avatar component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,15 @@
 import { Link } from 'react-router-dom';
+import Avatar from './Avatar';
 
 export default function Header() {
   return (
-    <header>
+    <header className='fixed top-0 left-0 right-0 flex bg-zinc-600'>
       <h3>ToDoApp</h3>
       <nav>
         <Link to='/'>Home</Link>
         <Link to='/about'>About</Link>
-        <Link to='/login'>Login</Link>
       </nav>
-      <div></div>
+      <Avatar />
     </header>
   );
 }
