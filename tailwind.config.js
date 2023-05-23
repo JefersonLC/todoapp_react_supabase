8beae3467f882b0d02d31d432c03f3c7c0378919
JefersonLC <c.jeferson.lc@gmail.com>
2023-05-22 23:08:35 -0500

    added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      russianViolet: '#23022e',
+      tekhelet: '#573280',
+      roseQuartz: '#ada8b6',
+      timberwolf: '#cecfc7',
+      honeydew: '#eff8e2',
+    },
   },
   plugins: [],
 };
