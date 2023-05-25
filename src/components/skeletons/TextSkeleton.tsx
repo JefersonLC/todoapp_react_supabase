f5729a3906e4da955fe290362602df946854b23b
JefersonLC <c.jeferson.lc@gmail.com>
2023-05-25 13:47:26 -0500

    updated colors

--- src/components/skeletons/TextSkeleton.tsx
+++ src/components/skeletons/TextSkeleton.tsx
@@ -3,7 +3,7 @@ export default function TextSkeleton({ length }: { length: string }) {
 
   return (
     <span
-      className={`block mx-2 my-1 bg-honeydew rounded-md h-2 animate-pulse ${skeletonStyle}`}
+      className={`block mx-2 my-1 bg-gray-300 rounded-md h-2 animate-pulse ${skeletonStyle}`}
     />
   );
 }
