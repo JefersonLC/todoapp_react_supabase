59b20188924c397be87fa30774290fe1cee5e153
JefersonLC <c.jeferson.lc@gmail.com>
2023-05-25 13:48:35 -0500

    updated useUser hook

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,12 +1,26 @@
 import { useContext } from 'react';
-import { Session } from '@supabase/supabase-js';
+import { PostgrestError, Session } from '@supabase/supabase-js';
 import { UserContext } from '../context/UserContext';
+
+export interface Task {
+  id: number;
+  title: string;
+  description: string;
+  created_at: string;
+  user_id: string;
+  limit_date: string;
+}
 
 interface Context {
   setSession: (session: Session) => void;
   signInWithGitHub: () => Promise<void>;
   signInWithGoogle: () => Promise<void>;
   signOut: () => Promise<void>;
+  getTasks: (uuid: string) => Promise<{
+    data: Task[] | null;
+    error: PostgrestError | null;
+    status: number;
+  }>;
   session: Session;
 }
 
