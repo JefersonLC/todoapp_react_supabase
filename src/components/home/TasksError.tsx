26789abb4c3c17c47c27ac9f013bfbe4dafe044a
JefersonLC <c.jeferson.lc@gmail.com>
2023-05-25 13:50:03 -0500

    task error component

---
+++ src/components/home/TasksError.tsx
@@ -0,0 +1,19 @@
+export default function TasksError() {
+  return (
+    <div className='mt-5 text-center text-white border rounded-sm px-4 py-2'>
+      <div className='mb-4'>
+        <h3 className='text-xl'>
+          Error <span className='text-teal-400'>400</span>
+        </h3>
+      </div>
+      <div className='text-lg'>
+        <p>There was an error during the process</p>
+      </div>
+      <div className='mt-4 text-sm'>
+        <p>
+          Please try again later <span className='text-teal-400'>;)</span>
+        </p>
+      </div>
+    </div>
+  );
+}
