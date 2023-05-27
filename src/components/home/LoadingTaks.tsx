dbb49ecb08b8050f519cd6fcc127ed1a67561920
JefersonLC <c.jeferson.lc@gmail.com>
2023-05-26 22:29:07 -0500

    loading

--- src/components/home/LoadingTaks.tsx
+++ src/components/home/LoadingTaks.tsx
@@ -1,15 +1,9 @@
-import TaskCardSkeleton from "../skeletons/TaskCardSkeleton";
+import { DotWave } from '@uiball/loaders';
 
 export default function LoadingTaks() {
   return (
-    <div className="mt-8">
-      <TaskCardSkeleton />
-      <TaskCardSkeleton />
-      <TaskCardSkeleton />
-      <TaskCardSkeleton />
-      <TaskCardSkeleton />
-      <TaskCardSkeleton />
-      <TaskCardSkeleton />
+    <div className='mt-8 flex justify-center py-60'>
+      <DotWave size={60} speed={1} color='white' />
     </div>
-  )
+  );
 }
