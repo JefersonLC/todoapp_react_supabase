f86d9f3b335102f39d3425513eee72af6d1122c6
JefersonLC <c.jeferson.lc@gmail.com>
2023-05-26 22:29:16 -0500

    task card

--- src/components/home/TaskCard.tsx
+++ src/components/home/TaskCard.tsx
@@ -1,10 +1,16 @@
+import { Link } from 'react-router-dom';
 import { Task } from '../../hooks/useUser';
 
-export default function TaskCard({ task }: { task: Task }) {
+interface TaskCardProps {
+  index: number;
+  task: Task;
+}
+
+export default function TaskCard({ index, task }: TaskCardProps) {
   return (
     <div className='bg-teal-400 rounded-sm px-2 py-3 min-w-[290px]'>
       <div className='px-4 flex items-center gap-2'>
-        <span className='text-[9px]'>#{task.id}</span>
+        <span className='text-[9px]'>#{index + 1}</span>
         <h5 className='flex-grow'>{task.title}</h5>
         <span className='text-[9px]'>
           {new Date(task.created_at).toLocaleDateString()}
@@ -14,10 +20,19 @@ export default function TaskCard({ task }: { task: Task }) {
         <div className='flex flex-col gap-2'>
           <p className='text-sm'>{task.description}</p>
           <p className='text-[9px]'>
-            Target: {new Date(task.limit_date).toLocaleDateString()}
+            Límite: {new Date(task.limit_date).toLocaleDateString()}
           </p>
         </div>
-        <div className='mt-2'>buttons</div>
+        <div className='mt-4 mb-2'>
+          <Link
+            to={'/update/' + task.id}
+            className={`px-4 py-2 bg-slate-900 rounded-sm text-teal-400
+            text-sm hover:bg-inherit hover:text-slate-900 hover:border
+            hover:border-slate-900`}
+          >
+            Modificar
+          </Link>
+        </div>
       </div>
     </div>
   );
