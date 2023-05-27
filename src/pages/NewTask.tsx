5a56fb986bfde55bedb3e14fd667c8c42399bc75
JefersonLC <c.jeferson.lc@gmail.com>
2023-05-27 13:55:13 -0500

    new task page

--- src/pages/NewTask.tsx
+++ src/pages/NewTask.tsx
@@ -1,15 +1,23 @@
-import { Field, Formik, Form } from "formik";
+import { Field, Formik, Form } from 'formik';
 
 export default function NewTask() {
+  const initialValues = {
+    title: '',
+  };
+
+  const handleSubmit = () => {
+    console.log('Test');
+  };
+
   return (
     <main className='min-h-screen pt-20 px-5 sm:px-20 md:px-36 lg:px-60 xl:px-96 bg-slate-700'>
       <div className='mt-6 mb-2 2xl:mx-56'>
         <h4 className='text-xl text-white'>Agrega una tarea:</h4>
       </div>
       <div className='mt-5 flex flex-col gap-4 2xl:mx-56'>
-        <Formik>
+        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
           <Form>
-            <Field type='text' name='title' placeholder='Título'/>
+            <Field type='text' name='title' placeholder='Título' />
           </Form>
         </Formik>
       </div>
