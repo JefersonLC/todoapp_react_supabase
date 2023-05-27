6ee27e5a913d7dc62b53f066af59eaa7e7fa0529
JefersonLC <c.jeferson.lc@gmail.com>
2023-05-26 22:31:16 -0500

    deleted

--- src/components/skeletons/TextSkeleton.tsx
+++
@@ -1,9 +0,0 @@
-export default function TextSkeleton({ length }: { length: string }) {
-  const skeletonStyle = `w-[${length}px]`;
-
-  return (
-    <span
-      className={`block mx-2 my-1 bg-gray-300 rounded-md h-2 animate-pulse ${skeletonStyle}`}
-    />
-  );
-}
