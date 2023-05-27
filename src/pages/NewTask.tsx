caf8cedb18ca60544cdb909b42559669c5431de3
JefersonLC <c.jeferson.lc@gmail.com>
2023-05-27 13:51:14 -0500

    new task page

--- src/pages/NewTask.tsx
+++ src/pages/NewTask.tsx
@@ -1,5 +1,18 @@
+import { Field, Formik, Form } from "formik";
+
 export default function NewTask() {
   return (
-    <div>NewTask</div>
-  )
+    <main className='min-h-screen pt-20 px-5 sm:px-20 md:px-36 lg:px-60 xl:px-96 bg-slate-700'>
+      <div className='mt-6 mb-2 2xl:mx-56'>
+        <h4 className='text-xl text-white'>Agrega una tarea:</h4>
+      </div>
+      <div className='mt-5 flex flex-col gap-4 2xl:mx-56'>
+        <Formik>
+          <Form>
+            <Field type='text' name='title' placeholder='Título'/>
+          </Form>
+        </Formik>
+      </div>
+    </main>
+  );
 }
