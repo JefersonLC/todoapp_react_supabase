bfb4ee9891afb0de0077ecf70d14251436117d66
JefersonLC <c.jeferson.lc@gmail.com>
2023-05-26 22:29:57 -0500

    add icon

---
+++ src/components/icons/AddIcon.tsx
@@ -0,0 +1,22 @@
+export default function AddIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 448 512'
+      className='w-6 h-6'
+      fill='currentColor'
+      aria-hidden='true'
+      width='40'
+      height='40'
+      strokeWidth='1.5'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    >
+      <path
+        d='M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7
+        0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3
+        32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z'
+      />
+    </svg>
+  );
+}
