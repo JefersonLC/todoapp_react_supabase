d3192e610c8774cb06456ae1ebed3e84941cf235
JefersonLC <c.jeferson.lc@gmail.com>
2023-05-28 19:26:34 -0500

    deleted CardButton.tsx

--- src/components/home/CardButton.tsx
+++
@@ -1,8 +0,0 @@
-interface CardButtonProps {
-  action: () => Promise<void> | void;
-  children: string;
-}
-
-export default function CardButton({ action, children }: CardButtonProps) {
-  return <button onClick={action}>{children}</button>;
-}
