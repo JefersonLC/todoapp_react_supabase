e03630fefe69040574bd161fcf97537d9666baf0
JefersonLC <c.jeferson.lc@gmail.com>
2023-05-28 21:16:23 -0500

    updated not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,25 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function NotFound() {
+  const { pathname } = useLocation();
+
   return (
-    <div>
-      NotFound
-      <br />
-      <Link to='/' className='text-green-800'>Go to Home</Link>
+    <div className='min-h-screen bg-slate-700 flex flex-col items-center justify-center text-center gap-4'>
+      <div className='text-white px-2'>
+        <h3 className='text-xl'>
+          Error <span className='text-teal-400'>404</span>
+        </h3>
+        <p className='mt-2'>
+          La ruta espeficicada no existe:{' '}
+          <span className='text-teal-400'>{pathname}</span>
+        </p>
+      </div>
+      <Link
+        to='/'
+        className='px-4 py-2 rounded-sm text-slate-900 bg-teal-400 hover:bg-slate-900 hover:text-teal-400'
+      >
+        Volver al inicio
+      </Link>
     </div>
   );
 }
