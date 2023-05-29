03b9f3e2b5d6827e6bc1677dfbb08070a0d5a1f7
JefersonLC <c.jeferson.lc@gmail.com>
2023-05-28 21:31:12 -0500

    updated SessionError component

--- src/components/home/SessionError.tsx
+++ src/components/home/SessionError.tsx
@@ -1,5 +1,12 @@
+import { Ring } from '@uiball/loaders';
+
 export default function SessionError() {
   return (
-    <div className='pt-20'>Cargando...</div>
-  )
+    <div className='min-h-screen flex items-center justify-center bg-slate-700'>
+      <div className='text-white text-center flex flex-col items-center gap-2'>
+        <h3 className='md:text-xl'>Conectando con la sesión...</h3>
+        <Ring color='white' />
+      </div>
+    </div>
+  );
 }
