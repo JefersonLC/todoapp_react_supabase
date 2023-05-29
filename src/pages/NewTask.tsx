444c0c83c627dc9ba8f9b58f2ddee61b2808c54f
JefersonLC <c.jeferson.lc@gmail.com>
2023-05-28 22:17:35 -0500

    test form

--- src/pages/NewTask.tsx
+++ src/pages/NewTask.tsx
@@ -1,12 +1,21 @@
 import { Field, Formik, Form } from 'formik';
+import { createTaskForm } from '../utils/validator';
 
 export default function NewTask() {
-  const initialValues = {
+  interface FormValues {
+    title: string;
+    description: string;
+    limit_date: string;
+  }
+
+  const initialValues: FormValues = {
     title: '',
+    description: '',
+    limit_date: '',
   };
 
-  const handleSubmit = () => {
-    console.log('Test');
+  const handleSubmit = (values: FormValues) => {
+    console.log(values);
   };
 
   return (
@@ -15,9 +24,16 @@ export default function NewTask() {
         <h4 className='text-xl text-white'>Agrega una tarea:</h4>
       </div>
       <div className='mt-5 flex flex-col gap-4 2xl:mx-56'>
-        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={createTaskForm}
+          onSubmit={handleSubmit}
+        >
           <Form>
             <Field type='text' name='title' placeholder='Título' />
+            <Field type='text' name='description' placeholder='Descripción' />
+            <Field type='date' name='limit_date' />
+            <button type='submit'>Agregar</button>
           </Form>
         </Formik>
       </div>
