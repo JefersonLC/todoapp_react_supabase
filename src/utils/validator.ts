b039065b2727242c75df0ac5a150ab97318aad36
JefersonLC <c.jeferson.lc@gmail.com>
2023-05-29 19:29:07 -0500

    updated validator messages

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -3,14 +3,14 @@ import * as Yup from 'yup';
 export const createTaskForm = Yup.object({
   title: Yup.string()
     .min(2, 'Muy corto')
-    .required('Campo obligatorio')
+    .required('Título obligatorio')
     .typeError('Sólo texto'),
   description: Yup.string()
     .min(2, 'Muy corto')
-    .required('Campo obligatorio')
+    .required('Descripción obligatoria')
     .typeError('Sólo texto'),
   limit_date: Yup.date()
     .min(new Date(), 'No permitido')
-    .required('Campo obligatorio')
-    .typeError('Debe se una fecha'),
+    .required('Fecha límite obligatoria')
+    .typeError('Debe ser una fecha'),
 });
