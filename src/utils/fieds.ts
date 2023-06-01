e01d62e0ee565ae36614ebf45175cc0e85042a01
JefersonLC <c.jeferson.lc@gmail.com>
2023-05-31 22:56:45 -0500

    forms fields

---
+++ src/utils/fieds.ts
@@ -0,0 +1,16 @@
+export const formFields = [
+  {
+    type: 'text',
+    name: 'title',
+    placeholder: 'Título',
+  },
+  {
+    type: 'text',
+    name: 'description',
+    placeholder: 'Descripción',
+  },
+  {
+    type: 'date',
+    name: 'limit_date',
+  },
+];
